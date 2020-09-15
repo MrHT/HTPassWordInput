7e2ecbff3c136ad34f9ad1ac01d3ba69808b6804
mac <>
2020-09-15 15:14:42 +0800

    no message

---
+++ HTPassWordInput/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTPassWordInput
+//
+//  Created by mac on 2020/9/15.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HTPassWordInput/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HTPassWordInput
+//
+//  Created by mac on 2020/9/15.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+@end
